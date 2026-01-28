65fe306f90ef419f96d52d9d213c162ec02b1ccd
Jackyno <jackynojeanbaptiste@gmail.com>
2026-01-28 13:28:49 -0500

    recuperation derniere modification URGmetEO

--- code-source/meteoAPI/routes/alerts.js
+++ code-source/meteoAPI/routes/alerts.js
@@ -21,15 +21,6 @@ const alertController = require('../controllers/alertController');
  *       500:
  *         description: Erreur serveur
  */
-// router.get('/', async (req, res) => {
-//   const { uuid } = req.query;
-//   try {
-//     const filter = uuid ? { deviceUUID: uuid } : {};
-//     const alerts = await Alert.find(filter).sort({ createdAt: -1 });
-//     res.json(alerts);
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// });
+
  router.get('/', alertController.getAlerts);
 module.exports = router;
